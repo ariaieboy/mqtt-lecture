1cb673152d76d66b67dc2e547ff4d522f3626e14
AriaieBOY <ariaieboy.ir@gmail.com>
2024-01-01 15:28:31 +0330

    add health check

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+Route::get('are-u-ok',fn()=>['ok']);
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('https://ariaieboy.ir');
 });
